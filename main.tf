d01bb92ebe66e5d3c397ac75c75a66d3887c7ea9
Steven Cherry <steve@cherryfamily.co.uk>
2025-03-19 12:38:13 +0000

    Added public subnets in different AZ's

--- main.tf
+++ main.tf
@@ -19,3 +19,24 @@ resource "aws_subnet" "c_private" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.subnet_cidr_c_private
 }
+
+resource "aws_subnet" "a_public" {
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet_cidr_a_public
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "b_public" {
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet_cidr_b_public
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "c_public" {
+  availability_zone       = data.aws_availability_zones.available.names[2]
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet_cidr_c_public
+  map_public_ip_on_launch = true
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,18 @@ variable "subnet_cidr_c_private" {
   type    = string
   default = "10.0.32.0/20"
 }
+
+variable "subnet_cidr_a_public" {
+  type    = string
+  default = "10.0.128.0/20"
+}
+
+variable "subnet_cidr_b_public" {
+  type    = string
+  default = "10.0.144.0/20"
+}
+
+variable "subnet_cidr_c_public" {
+  type    = string
+  default = "10.0.160.0/20"
+}
