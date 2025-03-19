82999fe0081d1c29d07d5b169719997713c891f4
Steven Cherry <steve@cherryfamily.co.uk>
2025-03-19 12:21:46 +0000

    Added private subnets in different AZ's

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
-resource "aws_vpc" "this" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "a_private" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_cidr_a_private
+}
+
+resource "aws_subnet" "b_private" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_cidr_b_private
+}
+
+resource "aws_subnet" "c_private" {
+  availability_zone = data.aws_availability_zones.available.names[2]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_cidr_c_private
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
 variable "vpc_cidr_block" {
-  type        = string
-  default     = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_a_private" {
+  type    = string
+  default = "10.0.0.0/20"
+}
+
+variable "subnet_cidr_b_private" {
+  type    = string
+  default = "10.0.16.0/20"
+}
+
+variable "subnet_cidr_c_private" {
+  type    = string
+  default = "10.0.32.0/20"
 }
